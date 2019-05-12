84ca6695d177db14c84cdbc288dc776e8958e1eb
gabrielruoff <geruoff@gmail.com>
2019-05-11 23:34:02 -0400

    Added proper java source and its respective compiled jar

---
+++ Compiled Jars/RACP/src/Joint.java
@@ -0,0 +1,18 @@
+
+public class Joint {
+	
+	public int position;
+
+	public Joint(int initialPos) {
+		
+		this.position = initialPos;
+		
+	}
+	
+	public void setPosition(int pos) {
+		
+		this.position = pos;
+		
+	}
+	
+}
